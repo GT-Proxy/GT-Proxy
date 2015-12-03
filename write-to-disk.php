95dfdfd6bc57f68819a3f3eddee9bcfa43351523
root <root@vtauri.linaresdigital.com>
2015-12-03 22:42:18 +0100

    Fix #1: Saving POST data to disk

---
+++ write-to-disk.php
@@ -0,0 +1,10 @@
+<?php
+if (!is_dir('log')) {
+  if (mkdir('log', true)) {
+    die("Could not create log dir\n");
+  }
+}
+$log = 'log/' . date('YmdHis') . '.log';
+file_put_contents($log, var_export($_POST, true));
+echo 'Done';
+?>
