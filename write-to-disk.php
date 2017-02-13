493bada7c45cdb89c49d12053df4382efb7c25f0
ojgarciab <oscar.github@linaresdigital.com>
2017-02-13 13:04:59 +0000

    Apply fixes from StyleCI

--- write-to-disk.php
+++ write-to-disk.php
@@ -1,10 +1,10 @@
 <?php
+
 if (!is_dir('log')) {
-  if (mkdir('log', true)) {
-    die("Could not create log dir\n");
-  }
+    if (mkdir('log', true)) {
+        die("Could not create log dir\n");
+    }
 }
-$log = 'log/' . date('YmdHis') . '.log';
+$log = 'log/'.date('YmdHis').'.log';
 file_put_contents($log, var_export($_POST, true));
 echo 'Done';
-?>
